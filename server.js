4aaae8f76c967f00eaecce8eb860568a180e1888
Stef <stephane.quint@gmail.com>
2015-10-21 14:19:39 +0200

    Changement  de server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get("/",function(req,res) {
-	res.send("Hello world1");
+	res.send("Hello world1222");
 })
 var port=Number(process.env.PORT || 5000);
 app.listen(port);
